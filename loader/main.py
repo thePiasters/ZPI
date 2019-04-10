1f19766d3090952b3481196c90b2877da8f15957
Shichi7 <aleksander.polawski@gmail.com>
2019-04-10 16:58:46 +0200

    struktura_projektu

--- loader/main.py
+++
@@ -1,5 +0,0 @@
-from Manager import Manager
-
-if __name__ == "__main__":
-    manager = Manager()
-    manager.run()
---
+++ main.py
@@ -0,0 +1,17 @@
+from loader.LoaderMain import LoaderMain
+
+
+def main():
+    name = "Zdzislaw"
+    surname = "Beksinski"
+
+    loader = LoaderMain(name, surname)
+    loader.run()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
