69cae757804542459fcc3166f394497a9f22109b
Piasta <katarzyna.piasta@intel.com>
2019-04-24 13:49:01 +0200

    merge

---
+++ main.py
@@ -0,0 +1,19 @@
+from manager.Manager import Manager
+from crawlers import magazyn_sztuki as ms
+
+
+def main():
+    name = "Zdzislaw"
+    surname = "Beksinski"
+
+    ms.find_painter_url(name+' '+surname)
+
+    manager = Manager(name, surname)
+    manager.run()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
