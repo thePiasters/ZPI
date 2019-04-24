51168537f64266bb2b523b89faf86962657f9605
Jedopan64 <48326673+Jedopan64@users.noreply.github.com>
2019-04-24 15:33:12 +0200

    z

---
+++ loader/LoaderMain.py
@@ -0,0 +1,15 @@
+class LoaderMain:
+
+    name  = ""
+    surname = ""
+
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+    def run(self):
+         #manager = loader.Manager()
+         #manager.run()
+        print("ok")
+
+
