11105ba7dbcd75481f71d9d251b071480be3f74f
Grzegorz Sawko <grzegorz-sawko@wp.pl>
2019-05-14 23:51:35 +0200

    Interface added

---
+++ Interfejs/WebApplication3/SearchResults.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
+namespace WebApplication3
+{
+    public partial class SearchResults : System.Web.UI.Page
+    {
+ 
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                BindList();
+        }
+
+        private void BindList()
+        {
+            List<MyImage> list = new List<MyImage>
+            {
+                new MyImage (1,"Content/Images/abstract.jpg","tit"),
+                new MyImage(2, "Content/Images/unnamed.jpg", "tit2"),
+                new MyImage (3,"Content/Images/abstract.jpg","tit3"),
+                new MyImage(4, "Content/Images/unnamed.jpg", "tit4"),
+                new MyImage (5,"Content/Images/abstract.jpg","tit"),
+                new MyImage (6,"Content/Images/abstract.jpg","tit"),
+                new MyImage(7, "Content/Images/unnamed.jpg", "tit2"),
+                new MyImage (8,"Content/Images/abstract.jpg","tit3"),
+                new MyImage(9, "Content/Images/unnamed.jpg", "tit4")
+            };
+            Repeater1.DataSource = list;
+            Repeater1.DataBind();
+
+        }
+
+
+    };
+
+}
